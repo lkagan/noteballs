960cc6957530bc0ae6315d39b1f2e2ff8cb09bcb
Larry Kagan <larry@superiocity.com>
2022-05-02 17:33:43 -0400

    Add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <RouterLink to="/">Notes</RouterLink> |
-  <RouterLink to="/stats">Stats</RouterLink>
+  <NavBar></NavBar>
   <RouterView />
 </template>
+
+<script setup>
+import NavBar from "@/components/Layout/NavBar.vue";
+</script>
+
+<style>
+@import 'bulma/css/bulma.min.css';
+</style>
+
