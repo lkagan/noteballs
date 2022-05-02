216d27f7feb455cf2d7e3db58d706542dd51d3c3
Larry Kagan <larry@superiocity.com>
2022-05-02 16:42:03 -0400

    Refactor routes into own module

--- src/main.js
+++ src/main.js
@@ -1,22 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createRouter, createWebHashHistory } from "vue-router"
+import router from '@/router'
 
-const router = createRouter({
-    history: createWebHashHistory(),
-    routes: [
-        {
-            path: '/',
-            name: 'notes',
-            component: () => import('@/views/ViewNotes.vue')
-        },
-        {
-            path: '/stats',
-            name: 'stats',
-            component: () => import('@/views/ViewStats.vue')
-        },
-    ]
-})
 
 createApp(App)
     .use(router)
