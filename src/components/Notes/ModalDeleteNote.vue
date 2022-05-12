375188aee68a3f5464014229b492cb5f51fa47d0
Larry Kagan <larry@superiocity.com>
2022-05-12 16:43:33 -0400

    Close 'delete' modal when clicking outside

--- src/components/Notes/ModalDeleteNote.vue
+++ src/components/Notes/ModalDeleteNote.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="modal is-active">
+  <div class="modal is-active p-4">
     <div class="modal-background"></div>
-    <div class="modal-card">
-      <header class="modal-card-head">
+    <div
+        class="modal-card"
+        ref="modalCardRef"
+    >
+    <header class="modal-card-head">
         <p class="modal-card-title">Delete Note</p>
         <button
             class="delete"
@@ -12,7 +15,6 @@
       </header>
       <section class="modal-card-body">
         Are you sure you want to delete this note?
-        {{modelValue}}
       </section>
       <footer class="modal-card-foot is-justify-content-flex-end">
         <button
@@ -28,6 +30,10 @@
 
 <script setup>
 
+// Imports
+import { onClickOutside } from "@vueuse/core";
+import { ref } from "vue";
+
 // Props
 const props = defineProps({
   modelValue: {
@@ -39,8 +45,15 @@ const props = defineProps({
 // Emits
 const emit = defineEmits(['update:modelValue']);
 
-// Methods
+// Close modal
 const closeModal = () => {
   emit('update:modelValue', false);
 }
+
+const modalCardRef = ref()
+
+onClickOutside(modalCardRef, () => {
+  closeModal();
+})
+
 </script>
